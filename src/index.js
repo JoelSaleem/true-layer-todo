aa8318d3a0216a8233cfecefdb5fdc3769d9c0f9
JoelSaleem <40210575+JoelSaleem@users.noreply.github.com>
2020-04-05 11:26:25 +0100

    Babel and Webpack set up

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+
+const App = () => {
+    return (
+        <div>Hello world</div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
